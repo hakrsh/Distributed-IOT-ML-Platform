9a51ba77bde3d1fa317df630bb280c4d1803d97f
hari <harikn77@gmail.com>
2022-03-19 20:46:56 +0530

    Deployer initial commit

---
+++ deployer/ai_deployer/aiDeployer.py
@@ -0,0 +1,39 @@
+import json
+
+def run(package):
+    import zipfile
+    with zipfile.ZipFile(package, 'r') as zip_ref:
+        zip_ref.extractall('ai_deployer')
+    print("Extracted package")
+    
+    contract = json.load(open('ai_deployer/model/model_contract.json'))
+    port = contract['port']
+    endpoint = contract['endpoint']
+    container_name = contract['name']
+    # generate server.py
+    server_code = ''
+    server_code += "from flask import Flask, request\n"
+    server_code += "import pickle\n"
+    server_code += "from preprocessing import preprocess\n"
+    server_code += "from postprocessing import postprocess\n"
+    server_code += "app = Flask(__name__)\n"
+    server_code += "\n"
+    server_code += "@app.route('/')\n"
+    server_code += "def index():\n"
+    server_code += "    return 'Model is running!'\n"
+    server_code += "\n"
+    server_code += f"@app.route('/{endpoint}', methods=['POST'])\n"
+    server_code += f"def {endpoint}():\n"
+    server_code += "    data = preprocess(request.json)\n"
+    server_code += "    modelfile = open('model.pkl', 'rb')\n"
+    server_code += "    model = pickle.load(modelfile)\n"
+    server_code += "    pred = model.predict(data)\n"
+    server_code += "    return postprocess(pred)\n"
+    server_code += "\n"
+    server_code += "if __name__ == \"__main__\":\n"
+    server_code += f"    app.run(host='0.0.0.0', port={int(port)})\n"
+
+    with open('ai_deployer/model/server.py', 'w') as f:
+        f.write(server_code)
+
+    return container_name
---
+++ deployer/app_deployer/appDeployer.py
@@ -0,0 +1,21 @@
+import json 
+
+def run(package,sensor_id):
+    import zipfile
+    with zipfile.ZipFile(package, 'r') as zip_ref:
+        zip_ref.extractall('app_deployer')
+    print("Extracted package")
+    contract = json.load(open('app_deployer/app/app_contract.json'))
+    container_name = contract['name']
+    # generate getdata.py inside app/src
+    sensorStub = ''
+    sensorStub += "import requests\n"
+    sensorStub += "import json\n"
+    sensorStub += 'def get_data():\n'
+    sensorStub += "    url = 'http://10.1.38.47:7000/data/" + sensor_id + "'\n"
+    sensorStub += "    r = requests.get(url)\n"
+    sensorStub += "    return json.loads(r.text)\n"
+    with open('app_deployer/app/src/getdata.py', 'w') as f:
+        f.write(sensorStub)
+
+    return container_name
