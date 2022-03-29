3c9bb95f4bd02afea0d0ebda0c2d1a64c423dbc6
hari <harikn77@gmail.com>
2022-03-29 16:20:08 +0530

    docker installation bash

---
+++ install-docker.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
+# Docker
+sudo apt remove --yes docker docker-engine docker.io containerd runc || true
+sudo apt update
+sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+sudo usermod --append --groups docker "$USER"
+sudo systemctl enable docker
+printf '\nDocker installed successfully\n\n'
+
+printf 'Waiting for Docker to start...\n\n'
+sleep 5
+
+# Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+printf '\nDocker Compose installed successfully\n\n'
