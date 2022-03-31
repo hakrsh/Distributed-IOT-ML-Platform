65f778461b8bebf2c99fa2e17931e65a2a86a808
hari <harikn77@gmail.com>
2022-03-31 16:57:14 +0530

    single tar file

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -1,3 +1,4 @@
+from tkinter import image_names
 import docker
 import json
 import logging
@@ -9,28 +10,17 @@ services = json.loads(open('config.json').read())
 logging.info('load config.json')
 
 client = docker.from_env()
-def build(image_name):
+output_tar_file = f"{services['tar_file']}_{services['version']}.tar"
+for service in services['services']:
+    image_name = f'{service["name"]}:{service["version"]}'
     logging.info('building image ' + image_name)
     image = client.images.build(path=service['path'], tag=image_name)[0]
     logging.info('build image ' + image_name)
-    logging.info('saving image ' + image_name)
-    with open(f'{image_name}.tar', 'wb') as f:
+    logging.info('adding image ' + image_name + ' to tar file ' + output_tar_file)
+    with open(output_tar_file, 'wb') as f:
         for chunk in image.save(chunk_size=1024):
             f.write(chunk)
-    logging.info('save image to file')
-    return image_name + '.tar'
-
-for service in services['services']:
-    image = f'{service["name"]}:{service["version"]}'
-    images = []
-    images.append(build(image))
-    
-output_tar_file = f"{services['tar_file']}_{service['version']}.tar"
-logging.info('creating tar file')
-with tarfile.open(output_tar_file, 'w') as tar:
-    for image in images:
-        tar.add(image)
-logging.info('tar file created')
+    logging.info('Added image ' + image_name + ' to tar file ' + output_tar_file)
     
 
 
