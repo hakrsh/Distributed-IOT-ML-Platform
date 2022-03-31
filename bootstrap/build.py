0311a38e8b81d6dc8739ca9cc8d4f4df249a986c
hari <harikn77@gmail.com>
2022-03-31 16:08:52 +0530

    bootstrap

---
+++ bootstrap/build.py
@@ -0,0 +1,19 @@
+import docker
+import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+services = json.loads(open('config.json').read())
+logging.info('load config.json')
+
+client = docker.from_env()
+for service in services['services']:
+    image = client.images.build(path=service['path'], tag=f'{service["name"]}:{service["version"]}')
+    logging.info('build image' + service['name']+':'+service['version'])
+    with open(f'{service["name"]}.json', 'w') as f:
+        for chunk in image.save(chunk_size=1024):
+            f.write(chunk)
+    logging.info('save image to file')
+
+
