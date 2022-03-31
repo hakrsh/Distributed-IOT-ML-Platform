ca1821b92a938dc59c9cb6197af5a921dc0a9e84
hari <harikn77@gmail.com>
2022-03-31 16:15:27 +0530

    build test

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -9,9 +9,9 @@ logging.info('load config.json')
 
 client = docker.from_env()
 for service in services['services']:
-    image = client.images.build(path=service['path'], tag=f'{service["name"]}:{service["version"]}')
-    logging.info('build image' + service['name']+':'+service['version'])
-    with open(f'{service["name"]}.json', 'w') as f:
+    image = client.images.build(path=service['path'], tag=f'{service["name"]}:{service["version"]}')[0]
+    logging.info('build image ' + service['name']+':'+service['version'])
+    with open(f'{service["name"]}_{service["version"]}.tar', 'wb') as f:
         for chunk in image.save(chunk_size=1024):
             f.write(chunk)
     logging.info('save image to file')
