959f037ce2c6fe7d4f933d9fd0191f3d46ff5268
hari <harikn77@gmail.com>
2022-04-01 01:13:53 +0530

    sensor port updates

--- sensor_manager/sensor_manager/__main__.py
+++ sensor_manager/sensor_manager/__main__.py
@@ -3,4 +3,4 @@ from sensor_manager.sensorManager import start_all_threads
 
 if __name__ == "__main__":
 	start_all_threads(producer, sensor_config)
-	app.run()
+	app.run(port=7005,host='0.0.0.0')
--- sensor_request_handler/sensor_request_handler/__main__.py
+++ sensor_request_handler/sensor_request_handler/__main__.py
@@ -1,4 +1,4 @@
 from sensor_request_handler.server import app
 
 if __name__ == "__main__":
-	app.run(port=9898,debug=True)
+	app.run(port=7000,host='0.0.0.0')
