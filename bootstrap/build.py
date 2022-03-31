d9262fb5e9b64a6455e6e190a59824cf5ded2ca1
hari <harikn77@gmail.com>
2022-03-31 18:29:41 +0530

    docker hub

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -8,17 +8,13 @@ services = json.loads(open('config.json').read())
 logging.info('load config.json')
 
 client = docker.from_env()
-output_tar_file = f"{services['tar_file']}_{services['version']}.tar"
+client.login(username=services['username'], password=services['password'])
 logging.info('Creating tar file')
-f = open(output_tar_file, 'wb')
+
 for service in services['services']:
     image_name = f'{service["name"]}:{service["version"]}'
     logging.info('building image ' + image_name)
     image = client.images.build(path=service['path'], tag=image_name)[0]
     logging.info('build image ' + image_name)
-    logging.info('adding image ' + image_name + ' to tar file ' + output_tar_file)
-    for chunk in image.save(chunk_size=1024):
-        f.write(chunk)
-    logging.info('Added image ' + image_name + ' to tar file ' + output_tar_file)
-f.close()
-logging.info('tar file created')
+    client.images.push(image_name)
+    logging.info('push image ' + image_name + ' to registry')
