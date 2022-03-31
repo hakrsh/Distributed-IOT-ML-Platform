b6c917e1c34dbc7011d2af8bf8b5e462d508d74f
hari <harikn77@gmail.com>
2022-03-31 16:44:47 +0530

    tar file test

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -1,5 +1,3 @@
-from concurrent.futures import thread
-from tkinter import image_names
 import docker
 import json
 import logging
@@ -11,26 +9,27 @@ services = json.loads(open('config.json').read())
 logging.info('load config.json')
 
 client = docker.from_env()
-def build(image):
-    logging.info('building image ' + image)
+def build(image_name):
+    logging.info('building image ' + image_name)
     image = client.images.build(path=service['path'], tag=image)[0]
-    logging.info('build image ' + image)
-    logging.info('saving image ' + image)
-    with open(f'{image}.tar', 'wb') as f:
+    logging.info('build image ' + image_name)
+    logging.info('saving image ' + image_name)
+    with open(f'{image_name}.tar', 'wb') as f:
         for chunk in image.save(chunk_size=1024):
             f.write(chunk)
     logging.info('save image to file')
-    return image + '.tar'
+    return image_name + '.tar'
 
 for service in services['services']:
     image = f'{service["name"]}:{service["version"]}'
     images = []
     images.append(build(image))
     output_tar_file = f"{services['tar_file']}_{service['version']}.tar"
+    logging.info('creating tar file')
     with tarfile.open(output_tar_file, 'w') as tar:
         for image in images:
             tar.add(image)
-
+    logging.info('tar file created')
     
 
 
