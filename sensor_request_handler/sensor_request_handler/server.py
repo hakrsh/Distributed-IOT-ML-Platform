a52805701a485bd66efc37787ad91a980dd5c6cc
Ubuntu <vm1@vm1.4raldueebajejnu2mmkrwhzseh.bx.internal.cloudapp.net>
2022-04-01 23:19:55 +0530

    Sensor request handler: json sensor data fix
    Signed-off-by: Ubuntu <vm1@vm1.4raldueebajejnu2mmkrwhzseh.bx.internal.cloudapp.net>

--- sensor_request_handler/sensor_request_handler/server.py
+++ sensor_request_handler/sensor_request_handler/server.py
@@ -1,3 +1,4 @@
+import ast
 from flask import Flask, jsonify
 import json
 import pymongo
@@ -7,42 +8,46 @@ from sensor_request_handler.config import module_config
 
 logging.basicConfig(level=logging.INFO)
 
-kafka_ip = module_config['kafka_ip']
-kafka_port = module_config['kafka_port']
+kafka_ip = module_config["kafka_ip"]
+kafka_port = module_config["kafka_port"]
 
 kafka_server = "{}:{}".format(kafka_ip, kafka_port)
 mongo_server = module_config["mongo_server"]
 
 client = pymongo.MongoClient(mongo_server)
-logging.info('Connected to database')
+logging.info("Connected to database")
 
-db = client[ "sensors" ]
+db = client["sensors"]
 sensor_config = db["sensordetails"]
-logging.info('Sensor database created')
+logging.info("Sensor database created")
 
 app = Flask(__name__)
 
+
 @app.route("/getAllSensors")
 def getAllSensors():
-	sensors_cursor = sensor_config.find({})
-	list_of_sensors = []
-	for document in sensors_cursor:
-			sensorinfo = {}
-			sensorinfo["sensor_id"] = document['topic_id']
-			sensorinfo["sensor_type"] = document['Type']
-			sensorinfo["sensor_location"] = document['Location']
-			list_of_sensors.append(sensorinfo)
-	return jsonify(list_of_sensors)
+    sensors_cursor = sensor_config.find({})
+    list_of_sensors = []
+    for document in sensors_cursor:
+        sensorinfo = {}
+        sensorinfo["sensor_id"] = document["topic_id"]
+        sensorinfo["sensor_type"] = document["Type"]
+        sensorinfo["sensor_location"] = document["Location"]
+        list_of_sensors.append(sensorinfo)
+    return jsonify(list_of_sensors)
 
 
 @app.route("/data/<sensor_id>")
 def getSensorData(sensor_id):
     print(sensor_id)
-    consumer = KafkaConsumer(sensor_id, bootstrap_servers=[kafka_server], enable_auto_commit=True)
+    consumer = KafkaConsumer(
+        sensor_id, bootstrap_servers=[kafka_server], enable_auto_commit=True
+    )
     logging.info("Connected to kafka")
     data = []
     for message in consumer:
-        print(message.value.decode('utf-8'))
-        data.append(message.value.decode('utf-8'))
+        print(message.value.decode("utf-8"))
+        data.append(ast.literal_eval(message.value.decode("utf-8")))
         break
-    return str(data)
+    return jsonify(data[0])
+
