00d1a27306cf736924450a504596da920bb29ae7
Jayaraj Balagopal <jayarajbalagopal@Jayarajs-MacBook-Air.local>
2022-04-02 22:38:12 +0530

    Fault tolerance: mongodb watch added
    Signed-off-by: Jayaraj Balagopal <jayarajbalagopal@Jayarajs-MacBook-Air.local>

--- monitor_ha/monitor_ha/__main__.py
+++ monitor_ha/monitor_ha/__main__.py
@@ -1,4 +1,4 @@
-from monitor_ha.app import run
+from monitor_ha.monitor import run
 
 if __name__ == "__main__":
 	run()
--- monitor_ha/monitor_ha/app.py
+++ monitor_ha/monitor_ha/app.py
@@ -1,9 +1,4 @@
-from monitor_ha.config import module_config
-import pymongo
-import logging
-import threading
-import time
-import docker
+from flask import Flask, render_template, request
 
 logging.basicConfig(level=logging.INFO)
 
@@ -18,41 +13,11 @@ db = client[ "repo" ]
 instances = db["instances"]
 logging.info('instance database created')
 
-def cleanup(client, instance_id, container_id):
-	logging.info("Cleaning up {}".format(container_id))
-	client.remove(container_id)
+@app.route('/start')
+def start():
+    return "started monitoring", 200
 
-def recover(instance_id):
-	logging.info("Recovering {}".format(container_id))
 
-def app_handler(app_info):
-	instance_id = app_info["instance_id"]
-	container_id = app_info["container_id"]
-	hostname = app_info["hostname"]
-	ip = app_info["ip"]
-	port = app_info["port"]
-	try:
-		client = docker.DockerClient(base_url="ssh://{}@{}".format(hostname, ip))
-	except Exception as e:
-		print("Could not connect to docker daemon")
-	while True:
-		try:
-			cont = client.containers.get(container_id)
-			cont_status = cont.status
-			if cont_status != "running":
-				logging.info("Container {} not running".format(container_id))
-				cleanup(client, container_id)
-				recover(instance_id)
-		except Exception as e:
-			print("Could not find any such container")
-		time.sleep(monitor_interval)
-
-def run():
-	instance_cursor = instances.find({})
-	thread_list = []
-	for document in instance_cursor:
-		thread = threading.Thread(target=app_handler, args=(document,))
-		thread_list.append(thread)
-		thread.start()
-	for thread in thread_list:
-		thread.join()
+@app.route('/stop')
+def start():
+    return "stop monitoring", 200
