82fd08677a28a07448f9c56517bad347d08bec1c
snehalkumar5 <snehal14201@gmail.com>
2022-04-02 12:31:30 +0530

    serv changes

--- mongo/server.py
+++ mongo/server.py
@@ -6,35 +6,41 @@ app = Flask(__name__)
 
 client = MongoClient('mongodb+srv://root:root@ias.tu9ec.mongodb.net/')
 db = client.users
-collection = db.tryusers
-# cid = collection.insert_one({'username': username,'roles':['ai-dev','app-dev'],'password':'1234'})
 
-# @app.route('/users/signup', methods=['GET','POST'])
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/users/signup', methods=['GET','POST'])
 def signup():
     if request.method == 'GET': 
-        return render_template('index.html')
+        return render_template('signup.html')
     elif request.method == 'POST':
-        # req = request['data']
-        # username, role, passwd = req.username, req.role, req.passwd
-        username = 'user1'
-        role='temp'
-        passwd= '1234'
+        req = request.get_data()
+        username, role, passwd = req.username, req.role, req.passwd
+        # username = 'user1'
+        # role='scheduler'
+        # passwd= '1234'
         collection = db[role]
-        cid = collection.insert_one({'username': username,'password':passwd})
-        tmp = collection.find_one({'username':username})
-        print(tmp)
+        tmp = collection.find_one({'username':username, 'password':passwd})
+        if tmp == None:
+            return 'Invalid'
         cid = collection.insert_one({'username': username,'password':passwd})
         print(cid)
         return 'Success'
 
 @app.route('/users/login', methods=['GET','POST'])
 def login():
-    pass
-@app.route('/users/logout',  methods=['GET' ,'POST'])
-def logout():
-    pass
-# cid = collection.insert_one({'username':'user2','role':'app-dev','email':'user1@gmail.com','password':'1234'})
+    if request.method == "GET":
+        return render_template('login.html')
+    elif request.method == 'POST':
+        req = request.get_data()
+        username, role, passwd = req.username, req.role, req.passwd
+        collection = db[role]
+        tmp = collection.find_one({'username':username, 'password':passwd})
+        if tmp == None:
+            return 'Invalid'
+        return 'Success'
 
 if __name__ == "__main__":
     app.run(debug=True, host='127.0.0.1', port=2500)
