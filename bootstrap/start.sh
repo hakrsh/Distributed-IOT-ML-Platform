c8bb1e767fdf4e945cead9571e88cc0a37a3ced6
hari <harikn77@gmail.com>
2022-04-05 16:05:40 +0530

    bootstrap: clean up

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -4,7 +4,9 @@ if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 fi
 sudo apt install sshpass jq -y
+sudo pip3 install docker paramiko
 
+echo "Reading server list..."
 master=$(cat servers.json | jq -r '.master')
 workers=$(cat servers.json | jq -r '.workers')
 IFS=',' ;
@@ -20,7 +22,7 @@ do
     
     sshpass -p $pass ssh -o 'StrictHostKeyChecking no' $user "mkdir -p .ssh"
     cat ~/.ssh/id_rsa.pub | sshpass -p $pass ssh $user  'cat >> .ssh/authorized_keys'
-    # docker Installation
+    echo "installing docker on $worker_ip"
     ssh $user 'wget https://gist.githubusercontent.com/097nitinkumar/f2ab08718f489df02f8b23f1e922e543/raw/ecec210efaa6eff98a0091e521e6daf8f06f08df/install-docker.sh'
     ssh $user 'chmod +x install-docker.sh'
     ssh $user './install-docker.sh'
@@ -31,36 +33,26 @@ user=`echo "${holder}" | head -1`
 pass=`echo "${holder}"| head -2 | tail -1`
 master_holder=$(echo $user | sed s/@/\\n/g)
 master_ip=`echo "${master_holder}"| head -2 | tail -1`
-#####################
-#making master password less
+
+echo "making master passwordless"
 sshpass -p $pass ssh -o 'StrictHostKeyChecking no' $user "mkdir -p .ssh"
 cat ~/.ssh/id_rsa.pub | sshpass -p $pass ssh $user  'cat >> .ssh/authorized_keys'
-# Installing necessary scripts
 ssh $user 'sudo apt-get update'
-# Installing python3
-ssh $user 'sudo apt-get install python3 -y'
-# Installing pip3
-ssh $user 'sudo apt-get install -y python3-pip'
-# Installing pip3-docker
+ssh $user 'sudo apt-get install python3 default-jre haproxy python3-pip -y'
 ssh $user 'pip3 install docker'
-#Installing docker on Master
+echo "installing docker on master"
 ssh $user 'wget https://gist.githubusercontent.com/097nitinkumar/f2ab08718f489df02f8b23f1e922e543/raw/ecec210efaa6eff98a0091e521e6daf8f06f08df/install-docker.sh'
 ssh $user 'chmod +x install-docker.sh'
 ssh $user './install-docker.sh'
-#Installing java for kafka
-# ssh $master 'sudo apt-get install default-jre -y'
-ssh $user 'sudo apt-get install default-jre -y'
+
 # Installing kafka
 # add check wheteher file exists or not (no need to download it again).
 ssh $user 'wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz'
 ssh $user 'tar -xzf kafka_2.13-3.1.0.tgz'
-# Note: We had to update config file also, in kafka (below)
-############uncomment below code later###############
-ssh $user 'cat ~/kafka_2.13-3.1.0/config/server.properties'
+ssh $user 'rm kafka_2.13-3.1.0.tgz'
+echo "Configuring kafka"
 ssh $user "echo 'advertised.listeners=PLAINTEXT://'$master_ip':9092' >> ~/kafka_2.13-3.1.0/config/server.properties"
 ssh $user "echo 'listeners=PLAINTEXT://0.0.0.0:9092' >> ~/kafka_2.13-3.1.0/config/server.properties"
-ssh $user 'cat ~/kafka_2.13-3.1.0/config/server.properties'
-############uncomment above code later###############
 
 # #######################Run at end#########################
 # # Run zookeeper for kafka (background)
@@ -69,20 +61,15 @@ ssh $user 'nohup kafka_2.13-3.1.0/bin/zookeeper-server-start.sh config/zookeeper
 ssh $user 'nohup kafka_2.13-3.1.0/bin/kafka-server-start.sh config/server.properties &'
 echo "Zookeeper and kafka started"
 # #######################Run at end#########################
-#Installing haproxy
-ssh $user 'sudo apt show haproxy'
-ssh $user 'sudo apt update'
-ssh $user 'sudo apt install -y haproxy'
-# #Note: We had to update config file also, in haproxy
-##############################################uncomment below code later###############
+
+echo "Configuring HAProxy"
 ssh $user 'sudo chmod 777 /etc/haproxy/haproxy.cfg'
-ssh $user 'cat /etc/haproxy/haproxy.cfg'
 ssh $user 'echo "frontend deployer_service">>/etc/haproxy/haproxy.cfg'
 ssh $user 'echo "  bind 127.0.0.1:9898">>/etc/haproxy/haproxy.cfg'
 ssh $user 'echo "  default_backend deployers">>/etc/haproxy/haproxy.cfg'
 ssh $user 'echo "backend deployers">>/etc/haproxy/haproxy.cfg'
 ssh $user 'echo "  balance roundrobin">>/etc/haproxy/haproxy.cfg'
-##############################################uncomment above code later###############
+
 
 IFS=',' ;
 count=2
@@ -94,11 +81,11 @@ do
 done
 ssh $user 'sudo chmod 644 /etc/haproxy/haproxy.cfg'
 ssh $user 'touch "install_sucess"'
-ssh $user 'cat /etc/haproxy/haproxy.cfg'
 ssh $user 'line="@reboot sleep 60 && /home/"$(whoami)"/start_script.sh";(crontab -u $(whoami) -l; echo "$line" ) | sudo crontab -u $(whoami) -'
 ssh $user 'wget https://gist.githubusercontent.com/nitin-kumar-iiith/e6b034c78690a37327e49d92381777c3/raw/fe56a6c8b21735d17b3702393f7948dd2ff8b23e/start_script.sh'
 ssh $user 'chmod +x start_script.sh'
 ssh $user './start_script.sh'
+
 echo "!!!!!!!!!!!!!!!!BUILD STARTED!!!!!!!!!!!!!!!!!!!!1"
 cd ../bootstrap
 python3 build.py
