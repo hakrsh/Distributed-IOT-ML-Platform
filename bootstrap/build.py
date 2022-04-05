6da8adb42394eeeb7a05bf228c799c1fb620c780
hari <harikn77@gmail.com>
2022-04-05 15:52:42 +0530

    bootstrap fix

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -1,11 +1,19 @@
 import docker
 import json
 import logging
+from jinja2 import Template
+
+def render_template(template_file, output_file, workers):
+    with open(template_file) as f:
+        template = Template(f.read())
+    with open(output_file, 'w') as f:
+        f.write(template.render(servers=workers))
 
 logging.basicConfig(level=logging.INFO)
 
-services = json.loads(open('config.json').read())
-logging.info('load config.json')
+logging.info('Reading config files')
+services = json.loads(open('services.json').read())
+servers = json.loads(open('servers.json').read())
 
 def build(host,path,image_tag,container_name):
     logging.info('Connecing to ' + host)
@@ -26,6 +34,8 @@ def build(host,path,image_tag,container_name):
     try:
         if container_name == 'deployer':
             client.containers.run(image_tag,name=container_name, detach=True, network='host', volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock', 'mode': 'rw'}})
+        elif container_name == "monitor_ha":
+            client.containers.run(image_tag,name=container_name, detach=True, network='host', volumes={'/home/vm1/.ssh': {'bind': '/root/.ssh', 'mode': 'rw'}})
         else:
             client.containers.run(image_tag,name=container_name, detach=True, network='host')
     except Exception as e:
@@ -33,24 +43,63 @@ def build(host,path,image_tag,container_name):
     logging.info('Started ' + container_name)
 
 
-for service in services['services']:
-    image_name = f'{service["name"]}:{service["version"]}'
-    logging.info('building image ' + image_name)
-    host = 'ssh://' + services['master']['user'] + '@' + services['master']['ip']
-    # host = 'unix://var/run/docker.sock'
-    if service['name'] == 'deployer':
-        for worker in services['workers']:
-            host = 'ssh://' + worker['user'] + '@' + worker['ip'] 
-            data = json.load(open('../deployer/deployer/config.json'))
-            data['host_ip'] = worker['ip']
-            data['host_name'] = worker['user']
-            with open('../deployer/deployer/config.json', 'w') as outfile:
-                json.dump(data, outfile)
-            logging.info('Updating config.json')
+def haproxy_config():
+    workers = []
+    for worker in servers['workers']:
+        temp = {}
+        temp['name'] = worker['user']
+        temp['ip'] = worker['ip']
+        workers.append(temp)
+    render_template('haproxy.j2', 'haproxy.cfg', workers=workers)
+
+def generate_service_config():
+    logging.info('Generating service config')
+    master_ip = servers['master']['ip']
+    workers = []
+    for worker in servers['workers']:
+        temp = {}
+        temp['name'] = worker['user']
+        temp['ip'] = worker['ip']
+        workers.append(temp)
+    service_config = {
+        "kafka_ip": master_ip,
+        "kafka_port": "9092",
+        "mongo_server": servers['mongo_server'],
+        "sensor_api": "http://" + master_ip + ":7000/",
+        "deployer_master": "http://" + master_ip + ":9999/",
+        "platform_manager": "http://" + master_ip + ":5000/",
+        "scheduler": "http://" + master_ip + ":8210/",
+        "workers": workers
+    }
+    logging.info('Writing service config')
+    for service in services['services']:
+        path = '../' + service['name'] + '/' + service['name'] + '/config.json'
+        with open(path, 'w') as outfile:
+            json.dump(service_config, outfile)
+        logging.info('Wrote service config to ' + path)
+    
+def start_service():
+    generate_service_config()
+    logging.info('Starting service')
+    for service in services['services']:
+        image_name = f'{service["name"]}:{service["version"]}'
+        logging.info('building image ' + image_name)
+        host = 'ssh://' + servers['master']['user'] + '@' + servers['master']['ip']
+        # host = 'unix://var/run/docker.sock'
+        if service['name'] == 'deployer':
+            for worker in servers['workers']:
+                host = 'ssh://' + worker['user'] + '@' + worker['ip'] 
+                data = json.load(open('../deployer/deployer/config.json'))
+                data['host_ip'] = worker['ip']
+                data['host_name'] = worker['user']
+                with open('../deployer/deployer/config.json', 'w') as outfile:
+                    json.dump(data, outfile)
+                logging.info('Updating config.json')
+                build(host,service['path'],image_name,service['name'])
+        elif service['name'] == 'system_monitor':
+            for worker in servers['workers']:
+                host = 'ssh://' + worker['user'] + '@' + worker['ip'] 
+                build(host,service['path'],image_name,service['name'])
+        else:
             build(host,service['path'],image_name,service['name'])
-    elif service['name'] == 'system_monitor':
-        for worker in services['workers']:
-            host = 'ssh://' + worker['user'] + '@' + worker['ip'] 
-            build(host,service['path'],image_name,service['name'])
-    else:
-        build(host,service['path'],image_name,service['name'])
+    logging.info('Platform has been deployed' + servers['master']['ip'] + ':2500')
---
+++ bootstrap/start.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+mkdir -p ~/.ssh
+if [ ! -f ~/.ssh/id_rsa ]; then
+    ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+fi
+sudo apt install sshpass jq -y
+
+master=$(cat servers.json | jq -r '.master')
+workers=$(cat servers.json | jq -r '.workers')
+IFS=',' ;
+worker_ips=""
+for i in $workers ; 
+do 
+    holder=$(echo $i | sed s/~/\\n/g)
+    user=`echo "${holder}" | head -1`
+    pass=`echo "${holder}"| head -2 | tail -1`
+    worker_holder=$(echo $user | sed s/@/\\n/g)
+    worker_ip=`echo "${worker_holder}"| head -2 | tail -1`
+    worker_ips=$worker_ip","$worker_ips
+    
+    sshpass -p $pass ssh -o 'StrictHostKeyChecking no' $user "mkdir -p .ssh"
+    cat ~/.ssh/id_rsa.pub | sshpass -p $pass ssh $user  'cat >> .ssh/authorized_keys'
+    # docker Installation
+    ssh $user 'wget https://gist.githubusercontent.com/097nitinkumar/f2ab08718f489df02f8b23f1e922e543/raw/ecec210efaa6eff98a0091e521e6daf8f06f08df/install-docker.sh'
+    ssh $user 'chmod +x install-docker.sh'
+    ssh $user './install-docker.sh'
+done
+# For Master
+holder=$(echo $master | sed s/~/\\n/g)
+user=`echo "${holder}" | head -1`
+pass=`echo "${holder}"| head -2 | tail -1`
+master_holder=$(echo $user | sed s/@/\\n/g)
+master_ip=`echo "${master_holder}"| head -2 | tail -1`
+#####################
+#making master password less
+sshpass -p $pass ssh -o 'StrictHostKeyChecking no' $user "mkdir -p .ssh"
+cat ~/.ssh/id_rsa.pub | sshpass -p $pass ssh $user  'cat >> .ssh/authorized_keys'
+# Installing necessary scripts
+ssh $user 'sudo apt-get update'
+# Installing python3
+ssh $user 'sudo apt-get install python3 -y'
+# Installing pip3
+ssh $user 'sudo apt-get install -y python3-pip'
+# Installing pip3-docker
+ssh $user 'pip3 install docker'
+#Installing docker on Master
+ssh $user 'wget https://gist.githubusercontent.com/097nitinkumar/f2ab08718f489df02f8b23f1e922e543/raw/ecec210efaa6eff98a0091e521e6daf8f06f08df/install-docker.sh'
+ssh $user 'chmod +x install-docker.sh'
+ssh $user './install-docker.sh'
+#Installing java for kafka
+# ssh $master 'sudo apt-get install default-jre -y'
+ssh $user 'sudo apt-get install default-jre -y'
+# Installing kafka
+# add check wheteher file exists or not (no need to download it again).
+ssh $user 'wget https://dlcdn.apache.org/kafka/3.1.0/kafka_2.13-3.1.0.tgz'
+ssh $user 'tar -xzf kafka_2.13-3.1.0.tgz'
+# Note: We had to update config file also, in kafka (below)
+############uncomment below code later###############
+ssh $user 'cat ~/kafka_2.13-3.1.0/config/server.properties'
+ssh $user "echo 'advertised.listeners=PLAINTEXT://'$master_ip':9092' >> ~/kafka_2.13-3.1.0/config/server.properties"
+ssh $user "echo 'listeners=PLAINTEXT://0.0.0.0:9092' >> ~/kafka_2.13-3.1.0/config/server.properties"
+ssh $user 'cat ~/kafka_2.13-3.1.0/config/server.properties'
+############uncomment above code later###############
+
+# #######################Run at end#########################
+# # Run zookeeper for kafka (background)
+ssh $user 'nohup kafka_2.13-3.1.0/bin/zookeeper-server-start.sh config/zookeeper.properties &'
+# # Run kafka (In Another Terminal)
+ssh $user 'nohup kafka_2.13-3.1.0/bin/kafka-server-start.sh config/server.properties &'
+echo "Zookeeper and kafka started"
+# #######################Run at end#########################
+#Installing haproxy
+ssh $user 'sudo apt show haproxy'
+ssh $user 'sudo apt update'
+ssh $user 'sudo apt install -y haproxy'
+# #Note: We had to update config file also, in haproxy
+##############################################uncomment below code later###############
+ssh $user 'sudo chmod 777 /etc/haproxy/haproxy.cfg'
+ssh $user 'cat /etc/haproxy/haproxy.cfg'
+ssh $user 'echo "frontend deployer_service">>/etc/haproxy/haproxy.cfg'
+ssh $user 'echo "  bind 127.0.0.1:9898">>/etc/haproxy/haproxy.cfg'
+ssh $user 'echo "  default_backend deployers">>/etc/haproxy/haproxy.cfg'
+ssh $user 'echo "backend deployers">>/etc/haproxy/haproxy.cfg'
+ssh $user 'echo "  balance roundrobin">>/etc/haproxy/haproxy.cfg'
+##############################################uncomment above code later###############
+
+IFS=',' ;
+count=2
+for i in $worker_ips ; 
+do
+    #make sure that $i is not void
+    ssh $user "echo '  server server$count $i:9898 check'>>/etc/haproxy/haproxy.cfg"
+    count=1+$count
+done
+ssh $user 'sudo chmod 644 /etc/haproxy/haproxy.cfg'
+ssh $user 'touch "install_sucess"'
+ssh $user 'cat /etc/haproxy/haproxy.cfg'
+ssh $user 'line="@reboot sleep 60 && /home/"$(whoami)"/start_script.sh";(crontab -u $(whoami) -l; echo "$line" ) | sudo crontab -u $(whoami) -'
+ssh $user 'wget https://gist.githubusercontent.com/nitin-kumar-iiith/e6b034c78690a37327e49d92381777c3/raw/fe56a6c8b21735d17b3702393f7948dd2ff8b23e/start_script.sh'
+ssh $user 'chmod +x start_script.sh'
+ssh $user './start_script.sh'
+echo "!!!!!!!!!!!!!!!!BUILD STARTED!!!!!!!!!!!!!!!!!!!!1"
+cd ../bootstrap
+python3 build.py
