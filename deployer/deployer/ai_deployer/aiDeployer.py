a0bf634912205d7e5bb8a8af57bb04a2425ec947
hari <harikn77@gmail.com>
2022-04-07 03:35:04 +0530

    deployer: add model.h5 support

--- deployer/deployer/ai_deployer/aiDeployer.py
+++ deployer/deployer/ai_deployer/aiDeployer.py
@@ -15,7 +15,6 @@ def run(package, model_id):
     # generate server.py
     server_code = ''
     server_code += "from flask import Flask, request\n"
-    server_code += "import pickle\n"
     server_code += "from preprocessing import preprocess\n"
     server_code += "from postprocessing import postprocess\n"
     server_code += "app = Flask(__name__)\n"
@@ -27,8 +26,13 @@ def run(package, model_id):
     server_code += f"@app.route('/{endpoint}', methods=['POST'])\n"
     server_code += f"def {endpoint}():\n"
     server_code += "    data = preprocess(request.json)\n"
-    server_code += "    modelfile = open('model.pkl', 'rb')\n"
-    server_code += "    model = pickle.load(modelfile)\n"
+    server_code += "    model=None\n"
+    server_code += "    try:\n"
+    server_code += "        import pickle\n"
+    server_code += "        model = pickle.load(open('model.pkl', 'rb'))\n"
+    server_code += "    except:\n"
+    server_code += "        from keras.models import load_model\n"
+    server_code += "        model = load_model('model.h5')\n"
     server_code += "    pred = model.predict(data)\n"
     server_code += "    return postprocess(pred)\n"
     server_code += "\n"
