e097ea1b27810fc9374aee223cf86b5c54e82b73
hari <harikn77@gmail.com>
2022-04-17 13:47:11 +0530

    update bootstrap script

--- bootstrap/start.sh
+++ bootstrap/start.sh
@@ -10,9 +10,16 @@ mkdir -p ~/.ssh
 if [ ! -f ~/.ssh/id_rsa ]; then
     ssh-keygen -q -N '' -t rsa -f ~/.ssh/id_rsa
 fi
-sudo apt -qq update && sudo apt -qq install python3 python3-pip sshpass jq -y > /dev/null
-pip3 install docker paramiko jinja2 > /dev/null
+echo "installing dependencies..."
+sudo apt -qq update && sudo apt -qq install python3 python3-pip sshpass jq -y > bootstrap.log
+pip3 install docker paramiko jinja2 >> bootstrap.log
 echo "installed dependencies - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+read -p "Do you want to rebuild the docker images? [y/N] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    python3 build_images.py
+    echo "rebuilt docker images - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
+fi
 read -p "Do you want to create new VMs? [y/n] " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -21,10 +28,10 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo "installing azure cli..."
-        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash > /dev/null
+        curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash >> bootstrap.log
         echo "Installed az-cli - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
         echo "Installing azure cli python..."
-        pip install azure-cli --upgrade > /dev/null
+        pip install azure-cli --upgrade >> bootstrap.log
         echo "Installed azure cli python - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
     fi
         az login
@@ -52,13 +59,13 @@ do
     # sudo -S sshpass -p $pass ssh -o 'StrictHostKeyChecking no' $user "mkdir -p .ssh"
     # cat ~/.ssh/id_rsa.pub | sudo sshpass -p $pass ssh $user  'cat >> .ssh/authorized_keys'
     sleep 2
-    sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $user > /dev/null
+    sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $user >> bootstrap.log
     echo "made $user password less - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
     sleep 1
     echo "installing docker on $worker_ip"
-    scp install-docker.sh $user:~/
+    scp install-docker.sh $user:~/ >> bootstrap.log
     ssh $user 'chmod +x install-docker.sh'
-    ssh $user './install-docker.sh' > /dev/null
+    ssh $user './install-docker.sh' >> bootstrap.log
 done
 echo "Installed docker on workers - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
 # For Master
@@ -72,23 +79,23 @@ echo "making $user passwordless"
 # sudo -S sshpass -p $pass ssh -o 'StrictHostKeyChecking no' $user "mkdir -p .ssh"
 # cat ~/.ssh/id_rsa.pub | sudo sshpass -p $pass ssh $user  'cat >> .ssh/authorized_keys'
 sleep 2
-sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $user > /dev/null
+sshpass -p $pass ssh-copy-id -o StrictHostKeyChecking=no $user >> bootstrap.log
 echo "$user now has passwordless access"
 sleep 1
 echo "installing docker on $user"
-ssh $user 'sudo apt-get -qq update' > /dev/null
-ssh $user 'sudo apt-get -qq install python3 default-jre python3-pip sshpass -y' > /dev/null
-ssh $user 'pip3 install docker ' > /dev/null
+ssh $user 'sudo apt-get -qq update' >> bootstrap.log
+ssh $user 'sudo apt-get -qq install python3 default-jre python3-pip sshpass -y' >> bootstrap.log
+ssh $user 'pip3 install docker ' >> bootstrap.log
 echo "installing docker on master"
-scp install-docker.sh $user:~/
+scp install-docker.sh $user:~/ >> bootstrap.log
 ssh $user 'chmod +x install-docker.sh'
-ssh $user './install-docker.sh' > /dev/null
+ssh $user './install-docker.sh' >> bootstrap.log
 echo "Installed docker on master - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
 
 echo "Installing kafka on master"
-scp install-kafka.sh $user:~/
+scp install-kafka.sh $user:~/ >> bootstrap.log
 ssh $user 'chmod +x install-kafka.sh'
-ssh $user 'sudo ./install-kafka.sh' > /dev/null
+ssh $user 'sudo ./install-kafka.sh' >> bootstrap.log
 echo "Installed kafka on master - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
 # prompt user to chose load balancer
 echo "Please choose a load balancer"
@@ -99,9 +106,9 @@ load_balancer=""
 if [ $choice -eq 1 ]; then
     load_balancer="haproxy"
     echo "Installing HAProxy"
-    ssh $user 'sudo apt-get -qq install haproxy -y' > /dev/null
+    ssh $user 'sudo apt-get -qq install haproxy -y' >> bootstrap.log
     python3 config_haproxy.py
-    scp haproxy.cfg $user:~/
+    scp haproxy.cfg $user:~/ >> bootstrap.log
     ssh $user 'sudo mv haproxy.cfg /etc/haproxy/haproxy.cfg'
     rm haproxy.cfg
     ssh $user 'sudo systemctl enable haproxy.service'
@@ -115,14 +122,14 @@ fi
 echo "Installed load balancer - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
 echo "making passwordless access to workers from master"
 python3 copy_ssh.py platform_config.json
-scp copy_ssh.sh $user:~/
+scp copy_ssh.sh $user:~/ >> bootstrap.log
 rm copy_ssh.sh
-ssh $user 'chmod +x copy_ssh.sh'
-ssh $user './copy_ssh.sh' > /dev/null
+ssh $user 'chmod +x copy_ssh.sh' >> bootstrap.log
+ssh $user './copy_ssh.sh' >> bootstrap.log
 echo "Made passwordless access to workers from master - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
-scp platform_config.json $user:~/
-scp services.json $user:~/
-scp -r ~/.azure $user:~/
+scp platform_config.json $user:~/ >> bootstrap.log
+scp services.json $user:~/ >> bootstrap.log
+scp -r ~/.azure $user:~/ >> bootstrap.log
 echo "!!!!!!!!!!!!!!!!BUILD STARTED!!!!!!!!!!!!!!!!!!!!"
 python3 build.py $load_balancer
 echo "Build completed - $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
--- platform_manager/platform_manager/app.py
+++ platform_manager/platform_manager/app.py
@@ -1,4 +1,3 @@
-from asyncio.log import logger
 import json
 import subprocess
 import threading
